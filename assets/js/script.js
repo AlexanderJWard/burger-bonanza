7f290c22eb76837a84ad2673f0f50895abdc07c4
AlexanderJWard <alexjamesward@hotmail.com>
2022-07-14 12:17:16 +0000

    Add event listener upon DOM Content Loaded for mouse clicks on button-type attribute. Generate 4 numbers between 1 and 4 and put into an array called numbers. Test with console.log(numbers) when start button pressed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
-function runGame() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByClassName("btn");
+    for (button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("button-type") === "start") {
+                runGame();
+            }
+        })
+    }
+})
 
+function runGame() {
+    generateQuestion();
 }
 
 function generateQuestion() {
-
+    let number1 = Math.floor(Math.random() * 4) + 1;
+    let number2 = Math.floor(Math.random() * 4) + 1;
+    let number3 = Math.floor(Math.random() * 4) + 1;
+    let number4 = Math.floor(Math.random() * 4) + 1;
+    let numbers = [number1, number2, number3, number4];
+    console.log(numbers);
 }
 
 function displayQuestion() {
