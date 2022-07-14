07e5fbb9cbd693a335d81938ea5852da98aa0993
AlexanderJWard <alexjamesward@hotmail.com>
2022-07-14 11:58:13 +0000

    Create barebones javascript function structure

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+function runGame() {
+
+}
+
+function generateQuestion() {
+
+}
+
+function displayQuestion() {
+
+}
+
+function hideQuestion() {
+
+}
+
+function displayAnswer() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function hideCheckbox() {
+
+}
+
+function displaySelection() {
+
+}
+
+function hideSelection() {
+
+}
+
+function startTimer() {
+
+}
+
+function resetGame() {
+
+}
+
+function incrementCorrect() {
+
+}
+
+function incrementIncorrect() {
+
+}
